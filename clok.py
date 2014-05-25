79c296a932113fb812adcd0c543de8e4fa8fca3d
Max Lehn <max@vomber.de>
2014-05-25 22:09:35 +0200

    Externalize configuration to config.py

--- clok.py
+++ clok.py
@@ -15,47 +15,13 @@ sys.path.append('../ht1632clib/python')
 from ht1632c import HT1632C
 from rotenc import RotEnc
 
+# import configuration
 sys.path.append('modules')
-from mod_clock import TimeModule
-from mod_clock import DateModule
-from mod_clock import DateModuleVertical
-from mod_clock import SecondBarModule
-from mod_weather import WeatherModule
-from mod_weather import WeatherModuleColored
-from mod_anniversary import AnniversaryModule
+import config
 
 # TODOs
 # - color/brightness schemes (ht1632c colormap/palette)
 # - logging
-
-# location settings
-CITY = 'Münster'
-COUNTRY = 'DE'
-
-# display settings
-NUM_PANELS = 2
-
-# calendar settings
-# Note: you will need a file 'mod_anniversary_credentials.dat' that
-# can be created using an external script by logging into your google
-# account and permitting mod_anniversary access to your calendar.
-# The module also needs the google calendar api to be installed.
-# This can usually be done by running
-#
-#   easy_install --upgrade google-api-python-client
-
-CALENDARS = [
-  {
-    'name' : '*',
-    'id' : 'someid@group.calendar.google.com',
-    'font' : (lambda disp: disp.font4x6)
-  },
-  {
-    'name' : 'Jahrestag',
-    'id' : 'someotherid@group.calendar.google.com',
-    'font' : (lambda disp: disp.font4x6)
-  }
-]
 
 # Locales should be set so that weekdays are displayed correctly
 locale.setlocale(locale.LC_ALL, 'de_DE.utf8')
@@ -65,62 +31,6 @@ PIN_ROTENC_1   = 3
 PIN_ROTENC_2   = 4
 PIN_ROTENC_BTN = 2
 
-# color codes
-COL_BLACK  = 0
-COL_GREEN  = 1
-COL_RED    = 2
-COL_ORANGE = 3
-
-SCREENS = [
-	{
-		# display rotation (multiples of 90° clockwise)
-		'rotation': 0,
-		# screen configuration
-		'screen': [
-			{ 'moduleFn': lambda disp: TimeModule(disp.font7x8num, COL_GREEN), 'x': 16, 'y': 0, 'w': 46, 'h': 8  },
-			{ 'moduleFn': lambda disp: SecondBarModule(COL_RED, COL_BLACK, 5, COL_ORANGE, 3), 'x': 0, 'y': 9, 'w': 64, 'h': 1  },
-			{ 'moduleFn': lambda disp: DateModule(disp.font4x5num, COL_GREEN), 'x': -1, 'y': 11, 'w': 46, 'h': 5  },
-			{ 'moduleFn': lambda disp: WeatherModule(CITY, COUNTRY, COL_ORANGE), 'x': 47, 'y': 11, 'w': 17, 'h': 5  },
-		]
-	},
-	{
-		'rotation': 0,
-		# screen configuration
-		'screen': [
-			{ 'moduleFn': lambda disp: TimeModule(disp.font7x8num, COL_GREEN), 'x': 16, 'y': 0, 'w': 31, 'h': 8  },
-			{ 'moduleFn': lambda disp: DateModuleVertical(disp.font3x4num, COL_GREEN), 'x': 0, 'y': 0, 'w': 16, 'h': 10  },
-			{ 'moduleFn': lambda disp: WeatherModuleColored(CITY, COUNTRY), 'x': 47, 'y': 0, 'w': 17, 'h': 5  },
-			{ 'moduleFn': lambda disp: AnniversaryModule(disp.font4x6, COL_GREEN, 100, CALENDARS), 'x': 0, 'y': 10, 'w': 64, 'h': 6  },
-		]
-	},
-	{
-		'rotation': 0,
-		'screen': [
-			{ 'moduleFn': lambda disp: TimeModule(disp.font12x16, COL_GREEN), 'x': 5, 'y': 1, 'w': 53, 'h': 15  },
-		]
-	},
-	{
-		'rotation': 1,
-		'screen': [
-			{ 'moduleFn': lambda disp: TimeModule(disp.font8x12, COL_ORANGE, "%H"), 'x': 1, 'y': 0, 'w': 15, 'h': 12  },
-			{ 'moduleFn': lambda disp: TimeModule(disp.font8x12, COL_ORANGE, "%M"), 'x': 1, 'y': 12, 'w': 15, 'h': 12  },
-			{ 'moduleFn': lambda disp: TimeModule(disp.font8x12, COL_GREEN, "%S"), 'x': 1, 'y': 24, 'w': 15, 'h': 12  },
-			{ 'moduleFn': lambda disp: WeatherModule(CITY, COUNTRY, COL_RED), 'x': -1, 'y': 48, 'w': 17, 'h': 5  },
-		]
-	},
-	{
-		# display rotation (multiples of 90° clockwise)
-		'rotation': 2,
-		# screen configuration
-		'screen': [
-			{ 'moduleFn': lambda disp: TimeModule(disp.font7x8num, COL_GREEN), 'x': 16, 'y': 0, 'w': 46, 'h': 8  },
-			{ 'moduleFn': lambda disp: SecondBarModule(COL_RED, COL_BLACK, 5, COL_ORANGE, 3), 'x': 0, 'y': 9, 'w': 64, 'h': 1  },
-			{ 'moduleFn': lambda disp: DateModule(disp.font4x5num, COL_GREEN), 'x': -1, 'y': 11, 'w': 46, 'h': 5  },
-			{ 'moduleFn': lambda disp: WeatherModule(CITY, COUNTRY, COL_ORANGE), 'x': 47, 'y': 11, 'w': 17, 'h': 5  },
-		]
-	},
-]
-
 CMD_EXIT = 0
 CMD_NEXTPROG = 1
 CMD_SETPWM = 2
@@ -129,11 +39,11 @@ command = CMD_EXIT
 pwmValue = 4
 
 def showScreen(screenId):
-	rotation = SCREENS[screenId]['rotation']
-	screen = SCREENS[screenId]['screen']
+	rotation = config.SCREENS[screenId]['rotation']
+	screen = config.SCREENS[screenId]['screen']
 	
 	# init display
-	disp = HT1632C(NUM_PANELS, rotation)
+	disp = HT1632C(config.NUM_PANELS, rotation)
 	disp.pwm(pwmValue)
 	
 	# main loop, process module updates
@@ -215,7 +125,7 @@ def showScreen(screenId):
 			print("Exiting.")
 			return -1
 		else:
-			nextScreen = (screenId + 1) % len(SCREENS)
+			nextScreen = (screenId + 1) % len(config.SCREENS)
 			print("Switching to screen %d" % nextScreen)
 			#command = CMD_EXIT
 			return nextScreen
---
+++ config.py
@@ -0,0 +1,94 @@
+# -*- coding: utf-8 -*-
+
+from mod_clock import TimeModule
+from mod_clock import DateModule
+from mod_clock import DateModuleVertical
+from mod_clock import SecondBarModule
+from mod_weather import WeatherModule
+from mod_weather import WeatherModuleColored
+from mod_anniversary import AnniversaryModule
+
+# location settings
+CITY = 'Münster'
+COUNTRY = 'DE'
+
+# display settings
+NUM_PANELS = 2
+
+# color codes
+COL_BLACK  = 0
+COL_GREEN  = 1
+COL_RED    = 2
+COL_ORANGE = 3
+
+# calendar settings
+# Note: you will need a file 'mod_anniversary_credentials.dat' that
+# can be created using an external script by logging into your google
+# account and permitting mod_anniversary access to your calendar.
+# The module also needs the google calendar api to be installed.
+# This can usually be done by running
+#
+#   easy_install --upgrade google-api-python-client
+
+CALENDARS = [
+  {
+    'name' : '*',
+    'id' : 'someid@group.calendar.google.com',
+    'font' : (lambda disp: disp.font4x6)
+  },
+  {
+    'name' : 'Jahrestag',
+    'id' : 'someotherid@group.calendar.google.com',
+    'font' : (lambda disp: disp.font4x6)
+  }
+]
+
+SCREENS = [
+	{
+		# display rotation (multiples of 90° clockwise)
+		'rotation': 0,
+		# screen configuration
+		'screen': [
+			{ 'moduleFn': lambda disp: TimeModule(disp.font7x8num, COL_GREEN), 'x': 16, 'y': 0, 'w': 46, 'h': 8  },
+			{ 'moduleFn': lambda disp: SecondBarModule(COL_RED, COL_BLACK, 5, COL_ORANGE, 3), 'x': 0, 'y': 9, 'w': 64, 'h': 1  },
+			{ 'moduleFn': lambda disp: DateModule(disp.font4x5num, COL_GREEN), 'x': -1, 'y': 11, 'w': 46, 'h': 5  },
+			{ 'moduleFn': lambda disp: WeatherModule(CITY, COUNTRY, COL_AUTO), 'x': 47, 'y': 11, 'w': 17, 'h': 5  },
+		]
+	},
+	{
+		'rotation': 0,
+		# screen configuration
+		'screen': [
+			{ 'moduleFn': lambda disp: TimeModule(disp.font7x8num, COL_GREEN), 'x': 16, 'y': 0, 'w': 31, 'h': 8  },
+			{ 'moduleFn': lambda disp: DateModuleVertical(disp.font3x4num, COL_GREEN), 'x': 0, 'y': 0, 'w': 16, 'h': 10  },
+			{ 'moduleFn': lambda disp: WeatherModuleColored(CITY, COUNTRY), 'x': 47, 'y': 0, 'w': 17, 'h': 5  },
+			{ 'moduleFn': lambda disp: AnniversaryModule(disp.font4x6, COL_GREEN, 100, CALENDARS), 'x': 0, 'y': 10, 'w': 64, 'h': 6  },
+		]
+	},
+	{
+		'rotation': 0,
+		'screen': [
+			{ 'moduleFn': lambda disp: TimeModule(disp.font12x16, COL_GREEN), 'x': 5, 'y': 1, 'w': 53, 'h': 15  },
+		]
+	},
+	{
+		'rotation': 1,
+		'screen': [
+			{ 'moduleFn': lambda disp: TimeModule(disp.font8x12, COL_ORANGE, "%H"), 'x': 1, 'y': 0, 'w': 15, 'h': 12  },
+			{ 'moduleFn': lambda disp: TimeModule(disp.font8x12, COL_ORANGE, "%M"), 'x': 1, 'y': 12, 'w': 15, 'h': 12  },
+			{ 'moduleFn': lambda disp: TimeModule(disp.font8x12, COL_GREEN, "%S"), 'x': 1, 'y': 24, 'w': 15, 'h': 12  },
+			{ 'moduleFn': lambda disp: WeatherModule(CITY, COUNTRY, COL_RED), 'x': -1, 'y': 48, 'w': 17, 'h': 5  },
+		]
+	},
+	{
+		# display rotation (multiples of 90° clockwise)
+		'rotation': 2,
+		# screen configuration
+		'screen': [
+			{ 'moduleFn': lambda disp: TimeModule(disp.font7x8num, COL_GREEN), 'x': 16, 'y': 0, 'w': 46, 'h': 8  },
+			{ 'moduleFn': lambda disp: SecondBarModule(COL_RED, COL_BLACK, 5, COL_ORANGE, 3), 'x': 0, 'y': 9, 'w': 64, 'h': 1  },
+			{ 'moduleFn': lambda disp: DateModule(disp.font4x5num, COL_GREEN), 'x': -1, 'y': 11, 'w': 46, 'h': 5  },
+			{ 'moduleFn': lambda disp: WeatherModule(CITY, COUNTRY, COL_ORANGE), 'x': 47, 'y': 11, 'w': 17, 'h': 5  },
+		]
+	},
+]
