5eb6b9006a216f2575f96bf0bf08044cb4261e9a
Max Lehn <max@vomber.de>
2014-05-25 20:50:24 +0200

    Merge WeatherModuleColored with WeatherModule

--- mod_weather.py
+++ mod_weather.py
@@ -10,6 +10,8 @@ COL_RED    = 2
 COL_ORANGE = 3
 COL_TRANSPARENT = 0xff
 
+COL_AUTO   = -1
+
 class WeatherModule:
 	# update interval (seconds)
 	interval = 900.0
@@ -32,39 +34,22 @@ class WeatherModule:
 				weather = self.owm.getcityweaterbyid(self.city)
 				temp = weather.getmaintempc()
 				print("Current temp: %4.1f°C" % temp)
-				putsSpecial(disp, x, y, "%4.1f^" % temp, font, self.col, 0)
+				if (self.col == COL_AUTO):
+					if (temp < 18):
+						col = COL_GREEN
+					elif (temp < 25):
+						col = COL_ORANGE
+					else:
+						col = COL_RED
+				else:
+					col = self.col
+				putsSpecial(disp, x, y, "%4.1f^" % temp, font, col, 0)
 			except:
 				print("Error: could not get temperature")
 			
-class WeatherModuleColored:
-	# update interval (seconds)
-	interval = 900.0
-	
+class WeatherModuleColored(WeatherModule):
 	def __init__(self, cityName, countryCode):
-		self.owm = OpenWeatherMapApi()
-		try:
-			cities = self.owm.getcitybycitycountrycode(cityName, None, countryCode)
-			self.city = cities[0].identifier if cities else None
-			print("OWM city ID for \"%s\" (%s): %d" % (cityName, countryCode, self.city))
-		except:
-			print("Error: could not get city \"%s\" (%s)" % (cityName, countryCode))
-			self.city = None
-	
-	def update(self, disp, x, y, w, h):
-		font = disp.font4x5num
-		if self.city != None:
-			try:
-				weather = self.owm.getcityweaterbyid(self.city)
-				temp = weather.getmaintempc()
-				print("Current temp: %4.1f°C" % temp)
-				if (temp < 18):
-					putsSpecial(disp, x, y, "%4.1f^" % temp, font, COL_GREEN, 0)
-				elif (temp < 25):
-					putsSpecial(disp, x, y, "%4.1f^" % temp, font, COL_ORANGE, 0)
-				else:
-					putsSpecial(disp, x, y, "%4.1f^" % temp, font, COL_RED, 0)
-			except:
-                                print("Error: could not get temperature")
+		WeatherModule.__init__(self, cityName, countryCode, COL_AUTO)
 
 
 # helper functions
